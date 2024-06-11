8b6d864f2a55f8871373aabffb45d07aef211004
wallace <exnesswallace@gmail.com>
2024-06-11 19:46:06 +0200

    made some corrections the session

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,10 +34,9 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ adding a user to the db
         """
-        session = self.__session
+        session = self._session
         new_user = User(email=email, hashed_password=hashed_password)
         session.add(new_user)
         session.commit()
         session.close()
-
         return new_user
