fa286a70554606bf3d27321a7e0f007f91457783
wallace <exnesswallace@gmail.com>
2024-06-17 14:14:21 +0200

    added a condition for path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,6 +17,8 @@ class Auth:
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
+        if not path.endswith("/"):
+            path += "/"
         if path in excluded_paths:
             return False
 
