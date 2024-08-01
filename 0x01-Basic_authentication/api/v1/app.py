1a4cdc0c8cfb80d4ae12325600badc471a4553b9
wallace <exnesswallace@gmail.com>
2024-08-01 18:00:02 +0200

    including auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,15 +4,12 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 
 auth = None
 if getenv('AUTH_TYPE') == 'basic_auth':
@@ -49,15 +46,15 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def authorized(error):
-    """ an unauthorized response page
+def unauthorized(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error):
-    """ a forbidden respomce page
+def forbidden(error) -> str:
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
